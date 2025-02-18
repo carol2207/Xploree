06cbce52367b036826fd3fb0b6b28378c7b0775e
RandyGraterol <soapdelinger@gmail.com>
2025-02-17 21:46:48 -0400

    cambios integrados ,secciones x3 ,responsive

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+/////////////////////////////////////////////////
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
+/////////////////////////////////////////////////
 router.get('/transporte',( req, res )=>{
   res.render('home/transporte')
 });
-
+////////////////////////////////////////////////
+router.get('/hospedaje',( req, res )=>{
+  res.render('home/hospedaje')
+});
+///////////////////////////////////////////////
+router.get('/turismo',( req, res )=>{
+  res.render('home/turismo')
+});
 module.exports = router;
 
